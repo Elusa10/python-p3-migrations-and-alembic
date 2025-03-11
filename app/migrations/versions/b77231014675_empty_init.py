fbd415dc0c15d9cc08536d8d2266f1a9d3ff2d23
Elusa10 <leslieelusa@gmail.com>
2025-03-12 00:15:55 +0300

    initial commit

---
+++ app/migrations/versions/b77231014675_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: b77231014675
+Revises: 
+Create Date: 2025-03-12 00:06:08.858818
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b77231014675'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
